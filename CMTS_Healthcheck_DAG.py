dfd14548b255d0feff8f18ea2441b870d293a154
ignaciogluck96 <159650610+ignaciogluck96@users.noreply.github.com>
2024-02-10 20:51:43 +0100

    Update CMTS_Healthcheck_DAG.py

--- CMTS_Healthcheck_DAG.py
+++ CMTS_Healthcheck_DAG.py
@@ -29,10 +29,7 @@
 ###########################################  READ ONLY LIBRARIES  ######################################################
 ###########################################                       ######################################################
 ########################################################################################################################
-import json
-import logging
-import os
-import sys
+import json, logging, os, sys
 sys.path.append(os.path.dirname(__file__))
 log = logging.getLogger(__name__)
 from datetime import datetime, timedelta
@@ -40,10 +37,10 @@ from airflow import DAG
 from airflow.operators import MyInstanceInitOperator, MyInstanceExitOperator, MyPythonOperator, DummyOperator, MyBranchOperator, MyAnsiblePlaybookOperator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.exceptions import AirflowException
-from my_lib import *
+from mylib import *
 from my_error_management import MyWorkflowErrorManagement
 from my_success_management import MyWorkflowSuccessManagement
-from my_runtime import * 
+from my_runtime import *
 ########################################################################################################################
 #######################################                                  ###############################################
 #######################################  CUSTOM LIBRARIES AND VARIABLES  ###############################################
@@ -52,7 +49,7 @@ from my_runtime import *
 
 try:
     import usecase
-except ImportError:
+except:
     pass
 
 ########################################################################################################################
@@ -68,7 +65,7 @@ default_args = {
     'provide_context': True
 }
 
-dag = DAG(dag_id='es4-88j-6iy', description='', start_date=datetime(2021,10,20), schedule_interval=None, catchup=False, on_failure_callback=MyWorkflowErrorManagement, on_success_callback=MyWorkflowSuccessManagement, default_args=default_args)
+dag = DAG(dag_id='MY_DAG', description='', start_date=datetime(2021,10,20), schedule_interval=None, catchup=False, on_failure_callback=MyWorkflowErrorManagement, on_success_callback=MyWorkflowSuccessManagement, default_args=default_args)
 
 
 ########################################################################################################################
@@ -76,19 +73,19 @@ dag = DAG(dag_id='es4-88j-6iy', description='', start_date=datetime(2021,10,20),
 ##############################################  CUSTOM FUNCTIONS  ######################################################
 ##############################################                    ######################################################
 ########################################################################################################################
-        
+
 def load_variables(**kwargs):
     job = kwargs.get("job")
     job.progress = 20
     form = getForm(kwargs)
-    
+
     inv = Inventory()
-    
-    # Key functions (kwargs, The name of the environment variable, The value of the environment variable)
+
+    # The key functions (kwargs, The name of the environment variable, The value of the environment variable)
     # Call as many times as needed for environment variables
     # setEnvironment(kwargs, "hosts", groupedHosts)
     # setRuntime(kwargs, "hosts", ips_list)
-    
+
     return True
 
 
@@ -98,93 +95,249 @@ def write_influx_partial_stats(**kwargs):
     task_path = MyDirectories(kwargs).task(task_id)
     job.log.info("Task Path: " + task_path)
 
-    # It now always reads the same file, then we need to make sure files are saved with name <hostname_variables.json> and insert/update the corresponding one
+    # Always read the same file for now, then files should be saved with name <hostname_variables.json> and insert/update the corresponding one
     files = glob.glob(task_path + "/*_variables.json")
-    
+
+
+
     for file in files:
         json_file = open(file, "r")
         contents = json_file.read()
-        variables = json.loads(contents)
-        
-        sh_env = eval(variables['sh_env']) #eval(variables['cmts_summary'][i])
-        cmts_summary = eval(variables['cmts_summary']) #eval(variables['cmts_summary'][i])
-        hostname = variables['hostname'] #variables['hostname'][i]
-        vendor = variables['vendor'] #variables['hostname'][i]
-        
-        nodes_affected_per_cmts = {}
-        nodes_not_affected_per_cmts = {}
-        pems_affected_per_cmts = {}
-        pems_not_affected_per_cmts = {}
-        
-        nodes_affected_per_cmts[hostname] = []
-        nodes_not_affected_per_cmts[hostname] = []
-        pems_affected_per_cmts[hostname] = []
-        pems_not_affected_per_cmts[hostname] = []
-        
-        # Missing loop to iterate through all hosts of the vendor
-        
-        # Node iteration
-        for interface in cmts_summary:
-            if interface['Offline'] == '':
-                interface['Offline'] = str(int(interface['Total']) - int(interface['Active']))
-            
-            if int(interface['Total']) != 0:
-                offline_percentage = int(float(int(interface['Offline'])) * 100 / float(int(interface['Total'])))
-            else:
-                offline_percentage = 0
-                
-            if offline_percentage >= 10:
-                nodes_affected_per_cmts[hostname].append(interface['INTERFACE'])
-            else:
-                nodes_not_affected_per_cmts[hostname].append(interface['INTERFACE'])
-                
-        for pem in sh_env:
-            if pem['state'] == 'WARNING':
-                pems_affected_per_cmts[hostname].append(pem['name'])
-            if pem['state'] == 'NORMAL':
-                pems_not_affected_per_cmts[hostname].append(pem['name'])
-            
-                
-        print(pems_affected_per_cmts)
-        print(pems_not_affected_per_cmts)
-        
-        setRuntime(kwargs, "nodes_affected_per_cmts_" + vendor, nodes_affected_per_cmts[hostname]) # Hardcoded, remove the hostname index and send by vendor
-        setRuntime(kwargs, "nodes_not_affected_per_cmts_" + vendor, nodes_not_affected_per_cmts[hostname])
-        setRuntime(kwargs, "pems_affected_per_cmts" + vendor, pems_affected_per_cmts[hostname]) # Hardcoded, remove the hostname index and send by vendor
-        setRuntime(kwargs, "pems_not_affected_per_cmts" + vendor, pems_not_affected_per_cmts[hostname])
-        
+        hosts = json.loads(contents)
+
+        cmts_list_per_vendor = []
+        nodes_per_cmts = {}
+        interfaces_per_cmts = {}
+
+        # For each host of the same vendor
+        for host in hosts:
+            cmts_summary = eval(host['cmts_summary'])
+            hostname = host['hostname']
+            vendor = host['vendor']
+
+            print("\nHostname: " + hostname)
+            #print("\nCMTS summary: " + str(cmts_summary))
+            print("\n"+ vendor)
+
+            cmts = {'hostname': hostname, 'interfaces': [], 'nodes': [], 'total': 0, 'active': 0, 'offline': 0, 'number_nodes_degraded': 0, 'number_nodes_affected': 0, 'number_nodes_optimum': 0}
+
+            nodes_per_cmts[hostname] = []
+            interfaces_per_cmts[hostname] = []
+
+            # Grouping and structure of a node
+            nodes = {}
+            interfaces = []
+            for parsed_interface in cmts_summary:
+
+                # Extracting data
+                interface_name = parsed_interface['INTERFACE']
+                total = int(parsed_interface['Total'])
+                active = int(parsed_interface['Active'])
+                if parsed_interface['Offline'] == '':
+                    offline = total - active
+                else:
+                    offline = int(parsed_interface['Offline'])
+
+                # Calculation of offline percentage of the interface
+                if total != 0:
+                    offline_percentage = int(float(offline) * 100 /  float(total))
+                else:
+                    offline_percentage = 0
+                interface = {'interface': interface_name, 'total': total, 'active': active, 'offline': offline, 'percentage': offline_percentage}
+                interfaces.append(interface)
+
+                # Adding data to the cmts dict
+                cmts['interfaces'].append(interface_name)
+                cmts['total'] += total
+                cmts['active'] += active
+                cmts['offline'] += offline
+                # Adding interface to the node dict
+                node_name = parsed_interface['Description']
+                if node_name == ' ' or node_name == '':
+                    node_name = 'NO_NAME'
+                if node_name not in nodes.keys():
+                    nodes[node_name] = {}
+                    node_interfaces = []
+                    node_interfaces.append(interface_name)
+                    nodes[node_name] = {'interfaces': node_interfaces, 'total': total, 'active': active, 'offline': offline}
+                else:
+                    nodes[node_name]['interfaces'].append(interface_name)
+                    nodes[node_name]['total'] += total
+                    nodes[node_name]['active'] += active
+                    nodes[node_name]['offline'] += offline
+
+            # Calculation of offline percentage of the nodes
+            for node_name, node_data in nodes.items():
+                if node_data['offline'] != 0:
+                    node_data['percentage'] = int(float(node_data['offline']) * 100 /  float(node_data['total']))
+                else:
+                    node_data['percentage'] = 0
+                if (node_data['percentage'] >= 10) and (node_data['percentage'] < 100):
+                    node_data['state'] = 'DEGRADED' #####################
+                    cmts['number_nodes_degraded'] += 1
+                elif node_data['percentage'] == 100:
+                    node_data['state'] = 'AFFECTED' #########################
+                    cmts['number_nodes_affected'] += 1
+                else:
+                    node_data['state'] = 'OPTIMUM'
+                    cmts['number_nodes_optimum'] += 1
+
+                cmts['nodes'].append(node_name)
+
+            nodes_per_cmts[hostname] = nodes
+            interfaces_per_cmts[hostname] = interfaces
+
+            # Calculation of offline percentage of the cmts
+            for node_name, node_data in nodes.items():
+                if cmts['offline'] != 0:
+                    cmts['percentage'] = int(float(cmts['offline']) * 100 /  float(cmts['total']))
+                else:
+                    cmts['percentage'] = 0
+
+            cmts_list_per_vendor.append(cmts)
+
+        print('\n\nnodes_per_cmts:\n')
+        print(json.dumps(nodes_per_cmts, indent=4))
+        print('\n\ninterfaces_per_cmts:\n')
+        print(json.dumps(interfaces_per_cmts, indent=4))
+        print('\n\ncmts_list_per_vendor:\n')
+        print(json.dumps(cmts_list_per_vendor, indent=4))
+
+        setRuntime(kwargs, "cmts_list_per_vendor_" + vendor, cmts_list_per_vendor)
+        setRuntime(kwargs, "nodes_per_vendor_" + vendor, nodes_per_cmts)
+        setRuntime(kwargs, "interfaces_per_vendor_" + vendor, interfaces_per_cmts)
+
     return True
-    
-    
+
+
 def write_influx_total_stats(**kwargs):
     job = kwargs.get("job")
     task_id = kwargs["task_instance"].task.task_id
     task_path = MyDirectories(kwargs).task(task_id)
     job.log.info("Task Path: " + task_path)
-    
-    nodes_affected = []
-    nodes_affected_per_vendor = {}
-    nodes_not_affected_per_vendor = {}
-    
-    total_nodes_affected_counter = 0
-    total_nodes_not_affected_counter = 0
-        
-    vendors = ['cisco','casa_systems','arris']
 
-    for vendor in vendors:
-        nodes_affected_per_vendor[vendor] = getRuntime(kwargs, "nodes_affected_per_cmts_" + vendor)
-        nodes_not_affected_per_vendor[vendor] = getRuntime(kwargs, "nodes_not_affected_per_cmts_" + vendor)
-    
-    tags = dict(description = 'CMTS Cisco Affection')
-    fields = {'OFFLINE': len(nodes_affected_per_vendor['cisco']), 'OK': len(nodes_not_affected_per_vendor['cisco'])}
-    mat_write_influx(measurement = 'CMTS Cisco Affection', tags=tags, fields=fields)
+    cmts_list_per_vendor = {}
+    nodes_per_vendor = {}
+    interfaces_per_vendor = {}
 
-    tags = dict(description = 'CMTS Casa Systems Affection')
-    fields = {'OFFLINE': len(nodes_affected_per_vendor['casa_systems']), 'OK': len(nodes_not_affected_per_vendor['casa_systems'])}
-    mat_write_influx(measurement = 'CMTS Casa Systems Affection', tags=tags, fields=fields)
+    for vendor in ['Vendor1', 'Vendor2', 'Vendor3']:
+        cmts_list_per_vendor[vendor] = getRuntime(kwargs, "cmts_list_per_vendor_" + vendor)
+        nodes_per_vendor[vendor] = getRuntime(kwargs, "nodes_per_vendor_" + vendor)
+        interfaces_per_vendor[vendor] = getRuntime(kwargs, "interfaces_per_vendor_" + vendor)
 
-    tags = dict(description = 'CMTS Arris Affection')
-    fields = {'OFFLINE': len(nodes_affected_per_vendor['arris']), 'OK': len(nodes_not_affected_per_vendor['arris'])}
-    mat_write_influx(measurement = 'CMTS Arris Affection', tags=tags, fields=fields)
-    
-    total_nodes_affected_counter = len(nodes_affected_per_vendor['cisco']) + len(nodes)
+    total_cmts = {'affected_list': [], 'number_cmts_affected': 0, 'number_cmts_optimum': 0, 'number_cmts_degraded': 0}
+
+    # Send data for each CMTS
+    exit = False
+    for vendor in ['Vendor1', 'Vendor2', 'Vendor3']:
+        for cmts in cmts_list_per_vendor[vendor]:
+            if cmts['percentage'] >= 10:
+                total_cmts['affected_list'].append(cmts['hostname'])
+                total_cmts['number_cmts_affected'] += 1
+                cmts['state'] = 'AFFECTED'
+            else:
+                if cmts['number_nodes_affected'] + cmts['number_nodes_degraded'] > 0:
+                    total_cmts['number_cmts_degraded'] += 1
+                    cmts['state'] = 'DEGRADED'
+                else:
+                    total_cmts['number_cmts_optimum'] += 1
+                    cmts['state'] = 'OPTIMUM'
+
+            tags = dict(VENDOR = vendor, HOSTNAME = cmts['hostname'])
+            fields = {'TOTAL MODEMS': cmts['total'], 'ONLINE': cmts['active'], 'OFFLINE': cmts['offline'], 'PERCENTAGE': cmts['percentage'], 'NODES NOT OPTIMUM': cmts['number_nodes_affected'] + cmts['number_nodes_degraded'], 'STATE': cmts['state']}
+            my_write_influx(measurement = 'PER CMTS STATUS', tags=tags, fields=fields)
+
+    print('\n\ntotal_cmts:\n')
+    print(json.dumps(total_cmts, indent=4))
+
+    tags = {}
+    fields = {'AFFECTED': total_cmts['number_cmts_affected'], 'DEGRADED': total_cmts['number_cmts_degraded'], 'OPTIMUM': total_cmts['number_cmts_optimum']}
+    my_write_influx(measurement = 'TOTAL CMTS STATUS', tags=tags, fields=fields)
+
+    # Send data per node
+    for vendor in ['Vendor1', 'Vendor2', 'Vendor3']:
+        for cmts_name, cmts_data in nodes_per_vendor[vendor].items():
+            for node_name, node_data in cmts_data.items():
+                tags = dict(VENDOR = vendor, HOSTNAME = cmts_name, NODE = node_name)
+                fields = {'TOTAL MODEMS': node_data['total'], 'ONLINE': node_data['active'], 'OFFLINE': node_data['offline'], 'PERCENTAGE': node_data['percentage'], 'STATE': node_data['state']}
+                my_write_influx(measurement = 'PER NODE STATUS', tags=tags, fields=fields)
+
+    # Send data per node
+    total_nodes_affected = 0
+    total_nodes_optimum = 0
+    total_nodes_degraded = 0
+    for vendor in ['Vendor1', 'Vendor2', 'Vendor3']:
+        for cmts in cmts_list_per_vendor[vendor]:
+            total_nodes_affected += cmts['number_nodes_affected']
+            total_nodes_optimum += cmts['number_nodes_optimum']
+            total_nodes_degraded += cmts['number_nodes_degraded']
+
+
+    tags = {}
+    fields = {'AFFECTED': total_nodes_affected, 'DEGRADED': total_nodes_degraded, 'OPTIMUM': total_nodes_optimum}
+    my_write_influx(measurement = 'TOTAL NODES STATUS', tags=tags, fields=fields)
+
+
+    return True
+################################################################################################################
+###############################################  BRANCHING  ####################################################
+################################################################################################################
+
+# detectCase = MyBranchOperator(task_id='Routine_Select', python_callable=detect_case, provide_context=True, dag=dag)
+
+########################################################################################################################
+###############################################                     ####################################################
+###############################################  TASKS DEFINITIONS  ####################################################
+###############################################                     ####################################################
+########################################################################################################################
+
+
+init = MyInstanceInitOperator(task_id='My_Initialize', dag=dag)
+
+Vendor1_CMTS = MyAnsiblePlaybookOperator(
+    task_id = 'Vendor1_CMTS',
+    playbook = 'vendor1_pb.yml',
+    load_environment = load_variables,
+    process_output = write_influx_partial_stats,
+    keep_run_files = True,
+    #limitQuery = f"data.networkRole.data.name=CMTS&data.vendorModel.data.vendor=Vendor1"
+    dag=dag
+)
+
+Vendor2_CMTS = MyAnsiblePlaybookOperator(
+    task_id = 'Vendor2_CMTS',
+    playbook = 'vendor2_pb.yml',
+    load_environment = load_variables,
+    process_output = write_influx_partial_stats,
+    keep_run_files = True,
+    #limitQuery = f"data.networkRole.data.name=CMTS&data.vendorModel.data.vendor=Vendor2"
+    dag=dag
+)
+
+Vendor3_CMTS = MyAnsiblePlaybookOperator(
+    task_id = 'Vendor3_CMTS',
+    playbook = 'vendor3_pb.yml',
+    load_environment = load_variables,
+    process_output = write_influx_partial_stats,
+    keep_run_files = True,
+    #limitQuery = f"data.networkRole.data.name=CMTS&data.vendorModel.data.vendor=Vendor3",
+    dag=dag
+)
+
+Get_Stats_CMTS = MyPythonOperator(
+    task_id = 'Get_Stats_CMTS',
+    python_callable=write_influx_total_stats,
+    dag=dag
+)
+
+
+end = MyInstanceExitOperator(task_id='My_Finalize', trigger_rule='one_success', dag=dag)
+
+
+########################################################################################################################
+################################################                  ######################################################
+################################################  TASKS WORKFLOW  ######################################################
+################################################                  ######################################################
+########################################################################################################################
+
+init >> [Vendor1_CMTS, Vendor2_CMTS, Vendor3_CMTS] >> Get_Stats_CMTS >> end
